aaeabff3dd82486be3c330930b7e24c3025c2d17
Gianluca De Rossi <gderossi@microsoft.com>
2020-04-21 12:25:51 +0200

    commit

--- runme.ps1
+++ runme.ps1
@@ -17,8 +17,8 @@ $secretValue = Read-Host -Prompt "Enter the virtual machine administrator passwo
 $resourceGroupName = "${projectName}rg"
 $keyVaultName = $projectName
 $adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
-$templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
-#$templateUri = "https://raw.githubusercontent.com/gderossilive/ARMLab/master/AKVdeploy.json?token=AEZ3DIYO5HPWPGJCBQST2GC6T3AU2"
+#$templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
+$templateUri = "https://raw.githubusercontent.com/gderossilive/ARMLab/master/AKVdeploy.json"
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -adUserId $adUserId -secretValue $secretValue
