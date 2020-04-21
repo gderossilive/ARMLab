8267629e4e11af9fb0aca816da11562aecee72ae
Gianluca De Rossi <gderossi@microsoft.com>
2020-04-21 16:33:01 +0200

    commit

--- runme.ps1
+++ runme.ps1
@@ -9,7 +9,8 @@ $connect=Connect-AzAccount
 $context = Get-AzSubscription -SubscriptionId $SubscriptionID
 Set-AzContext $context
 
-$projectName = "gdr"+(Get-Random)
+$projectName = "gdr1274793862"
+#$projectName = "gdr"+(Get-Random)
 $location = "westeurope"
 $upn=$connect.Context.Account.id
 $secretValue = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
@@ -17,10 +18,16 @@ $secretValue = Read-Host -Prompt "Enter the virtual machine administrator passwo
 $resourceGroupName = "${projectName}rg"
 $keyVaultName = $projectName
 $adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
-#$templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
 $templateUri = "https://raw.githubusercontent.com/gderossilive/ARMLab/master/AKVdeploy.json"
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -adUserId $adUserId -secretValue $secretValue
+
+Write-Host "Press [ENTER] to continue ..."
+
+$templateUri = "https://raw.githubusercontent.com/gderossilive/ARMLab/master/AzureVMdeploy.json"
+$templatefile = "C:\Users\gderossi\OneDrive - Microsoft\Poste Italiane\PCL\GitHub\ARMlab\AzureVMdeploy.parameters.json"
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatefile -TemplateUri $templateUri
 
 Write-Host "Press [ENTER] to continue ..."
