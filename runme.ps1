d28974123019d4514b286865711028087e5a048f
Gianluca De Rossi <gderossi@microsoft.com>
2020-04-21 11:24:56 +0200

    commit

---
+++ runme.ps1
@@ -0,0 +1,14 @@
+$projectName = "gdr"+(Get-Random)
+$location = "westeurope"
+$upn = "gderossi@microsoft.com"
+$secretValue = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
+
+$resourceGroupName = "${projectName}rg"
+$keyVaultName = $projectName
+$adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
+$templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -adUserId $adUserId -secretValue $secretValue
+
+Write-Host "Press [ENTER] to continue ..."
