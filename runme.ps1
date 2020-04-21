ab0b12c62127ed9d2329adf7f81b97232b8eba68
Gianluca De Rossi <gderossi@microsoft.com>
2020-04-21 12:16:46 +0200

    reset

--- runme.ps1
+++ runme.ps1
@@ -1,12 +1,24 @@
+[CmdletBinding()]
+param ( 
+    [Parameter(Mandatory=$True)]
+    [String]
+    $SubscriptionID
+)
+
+$connect=Connect-AzAccount
+$context = Get-AzSubscription -SubscriptionId $SubscriptionID
+#Set-AzContext $context
+
 $projectName = "gdr"+(Get-Random)
 $location = "westeurope"
-$upn = "gderossi@microsoft.com"
+$upn=$connect.Context.Account.id
 $secretValue = Read-Host -Prompt "Enter the virtual machine administrator password" -AsSecureString
 
 $resourceGroupName = "${projectName}rg"
 $keyVaultName = $projectName
 $adUserId = (Get-AzADUser -UserPrincipalName $upn).Id
 $templateUri = "https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/tutorials-use-key-vault/CreateKeyVault.json"
+#$templateUri = "https://raw.githubusercontent.com/gderossilive/ARMLab/master/AKVdeploy.json?token=AEZ3DIYO5HPWPGJCBQST2GC6T3AU2"
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -keyVaultName $keyVaultName -adUserId $adUserId -secretValue $secretValue
